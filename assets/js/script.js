cfd2a6634b6d8f54ab0ca83f8d3441fbf2664c7e
SKYDOO2\Developer <wojciech.musial@skydoo.com>
2023-01-09 13:33:06 +0100

    responsive popup menu on mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,25 @@
-$('i.menu-trigger').click(function() {
-    $('.header__nav-bar nav').slideToggle();
-    console.log('clicked');
-});
+// DESKTOP TRIGGER MENU
+jQuery.fn.openPopup = function() {
+  $(this).css('display','block');
+    return this; 
+};
+
+jQuery.fn.closePopup = function() {
+    $(this).css('display','none');
+      return this; 
+  };
+
+
+if ($(window).width() > 952) { 
+    $('i.menu-trigger').click(function() {
+        $('.header__nav-bar nav').slideToggle();
+    });
+} else {
+    $('i.menu-trigger').click(function() {
+       $('#popup-menu').openPopup();
+    });
+}
+
+$('.popup-menu i, .popup-menu a').click(function() {
+    $('#popup-menu').closePopup();
+})
