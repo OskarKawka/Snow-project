7ce5fd25955565cdf1d7612d5083b1444a9ac3c7
SKYDOO2\Developer <wojciech.musial@skydoo.com>
2023-01-09 10:02:53 +0100

    making slider responsive

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -2,21 +2,21 @@ var btn = document.getElementsByClassName("btnn");
 var slide = document.getElementById("slide");
 
 btn[0].onclick = function() {
-    slide.style.transform = "translateX(0px)";
+    slide.style.transform = "translateX(0%)";
     for (i=0; i<3; i++) {
         btn[i].classList.remove("active");
     }
     this.classList.add("active");
 }
 btn[1].onclick = function() {
-    slide.style.transform = "translateX(-800px)";
+    slide.style.transform = "translateX(-100%)";
     for (i=0; i<3; i++) {
         btn[i].classList.remove("active");
     }
     this.classList.add("active");
 }
 btn[2].onclick = function() {
-    slide.style.transform = "translateX(-1600px)";
+    slide.style.transform = "translateX(-200%)";
     for (i=0; i<3; i++) {
         btn[i].classList.remove("active");
     }
