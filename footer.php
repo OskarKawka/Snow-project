cf257d4830da48efedd1cb636d85a627f3d20c9e
SKYDOO2\Developer <wojciech.musial@skydoo.com>
2023-01-09 15:48:34 +0100

    slice html to php files

---
+++ footer.php
@@ -0,0 +1,182 @@
+
+    <footer>
+        <section class="contact" id="contact">
+            <div class="container">
+                <div class="row">
+                    <div class="contact-info col-sm-6">
+                        <h4 class="contact-info__title">
+                            Contact Info:
+                        </h4>
+                        <p class="contact-info__desc">
+                            Integer vestibulum turpis vitae elit fermentum, quis vulputate leo rhoncus. Duis ullamcorper
+                            lorem sit amet est rutrum tempor.
+                        </p>
+                        <a href="www.google.com" class="link">
+                            <div class="contact-info__adress">
+                                <span class="bold">Adress: </span><span class="data">1011 Santa Monica Boulevard,
+                                    LA</span>
+                            </div>
+                        </a>
+                        <a href="tel:+44 928 838 756" class="link">
+                            <div class="contact-info__details">
+                                <span class="bold">Phone: </span> <span class="data">+44 928 838 756</span>
+                            </div>
+                        </a>
+                        <a href="mailto:info@example.com" class="link">
+                            <div class="contact-info__details">
+                                <span class="bold">Email: </span><span class="data">info@example.com</span>
+                            </div>
+                        </a>
+                        <a href="tel:+44 139 999 003" class="link">
+                            <div class="contact-info__details">
+                                <span class="bold">Fax: </span><span class="data">+44 139 999 003</span>
+                            </div>
+                        </a>
+
+                    </div>
+                    <div class="form col-sm-6">
+                        <form action="/my-handling-form-page" method="post">
+                            <ul>
+                                <li class="form__name">
+                                    <label for="name"></label>
+                                    <input type="text" id="name" name="user_name" placeholder="Your Name">
+                                </li>
+                                <li class="form__mail">
+                                    <label for="mail"></label>
+                                    <input type="email" id="mail" name="user_email" placeholder="Your Email">
+                                </li>
+                                <li class="form__title">
+                                    <label for="title"></label>
+                                    <input type="title" id="title" name="title" placeholder="Your Title">
+                                </li>
+                                <li class="form__msg">
+                                    <label for="msg"></label>
+                                    <textarea id="msg" name="user_message" placeholder="Your Comment"></textarea>
+                                </li>
+                                <li class="button">
+                                    <button type="submit">SEND MESSAGE</button>
+                                </li>
+                            </ul>
+                        </form>
+                    </div>
+
+
+                </div>
+            </div>
+
+        </section>
+        <section class="links">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-4">
+                        <h5 class="links__title">
+                            About Us
+                        </h5>
+                        <div class="links__desc">
+                            <p>
+                                Donec nec leo blandit, luctus elit viverra, rhoncus mauris. Curabitur sit amet tempus
+                                urna, ac vestibulum odio.
+                            </p>
+                            <div class="links__social-icons">
+                                <ul class="social-icons">
+                                    <li><i class="fa-brands fa-facebook-f"></i></li>
+                                    <li><i class="fa-brands fa-twitter"></i></li>
+                                    <li><i class="fa-brands fa-google-plus-g"></i></li>
+                                    <li><i class="fa-brands fa-pinterest"></i></li>
+                                    <li><i class="fa-brands fa-instagram"></i></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <h5 class="links__title">
+                            Latest Twits
+                        </h5>
+                        <div class="links__desc">
+                            <div class="links__tweet-row">
+                                <div class="links__tweet-icon">
+                                    <i class="fa-brands fa-twitter"></i>
+                                </div>
+                                <div class="links__tweet-desc">
+                                    <p>Donec nec leo blandit, luctus elit viverra, rhoncus mauris. </p>
+                                </div>
+                            </div>
+                            <div class="links__tweet-row">
+                                <div class="links__tweet-icon">
+                                    <i class="fa-brands fa-twitter"></i>
+                                </div>
+                                <div class="links__tweet-desc">
+                                    <p>Donec nec leo blandit, luctus elit viverra, rhoncus mauris. </p>
+                                </div>
+                            </div>
+                            <div class="links__tweet-row">
+                                <div class="links__tweet-icon">
+                                    <i class="fa-brands fa-twitter"></i>
+                                </div>
+                                <div class="links__tweet-desc">
+                                    <p>Donec nec leo blandit, luctus elit viverra, rhoncus mauris. </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <h5 class="links__title">
+                            Instagram
+                        </h5>
+                        <div class="links__insta">
+                            <div class="links__insta-img">
+                                <img src="assets/images/insta1.png" alt="">
+                            </div>
+                            <div class="links__insta-img">
+                                <img src="assets/images/insta2.jpg" alt="">
+                            </div>
+                            <div class="links__insta-img">
+                                <img src="assets/images/insta3.jpeg" alt="">
+                            </div>
+                            <div class="links__insta-img">
+                                <img src="assets/images/insta4.jpeg" alt="">
+                            </div>
+                            <div class="links__insta-img">
+                                <img src="assets/images/insta5.jpeg" alt="">
+                            </div>
+                            <div class="links__insta-img">
+                                <img src="assets/images/insta6.png" alt="">
+                            </div>
+                            <div class="links__insta-img">
+                                <img src="assets/images/insta7.jpeg" alt="">
+                            </div>
+                            <div class="links__insta-img">
+                                <img src="assets/images/insta8.jpeg" alt="">
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <section class="autor">
+                <div class="autor__text">
+                    <p>@2022. Created by Oskar Kawka</p>
+                </div>
+            </section>
+
+
+        </section>
+    </footer>
+
+
+
+    <!-- START: Scripts -->
+
+    <script src="assets/js/script.js"></script>
+    <script src="assets/js/slider.js"></script>
+    <script src="node_modules/bootstrap/dist/js/bootstrap.js"></script>
+
+
+
+
+    <!-- END: Scripts -->
+
+
+</body>
+
+</html>
